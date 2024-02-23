e5d556ecfc2a36382c43b7d8c340601f8c8555e3
Murad Arfanian <smurkensons@gmail.com>
2024-02-23 05:41:14 +0000

    feat: added example for pre_hook

--- macros/get_payment_type_description.sql
+++ macros/get_payment_type_description.sql
@@ -24,3 +24,20 @@ This macro returns the description of the payment_type #}
         then 'Voided trip'
     end
 {% endmacro %}
+
+
+{% macro create_table_tmp(in_schema_nm, in_table_nm) %}
+
+        create table if
+        not exists {{ in_schema_nm }}.{{ in_table_nm }}(
+            album_id int64,
+            id int64,
+            title string,
+            url string,
+            thumbnail_url string,
+            _dlt_load_id string not null,
+            _dlt_id string not null
+        )
+    ;
+
+{% endmacro %}
